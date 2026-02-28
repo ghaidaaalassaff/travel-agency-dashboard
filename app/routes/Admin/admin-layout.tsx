804ea48c46e348fd6636be6ddc149c614d0b13b2
Ghaidaa <Ghaidaa@nafith.com>
2026-02-28 05:15:21 +0300

    Routing

---
+++ app/routes/Admin/admin-layout.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Outlet} from "react-router";
+
+function AdminLayout() {
+    return (
+        <div className="admin-layout">MobileSideBar
+            <aside className="w-full max-w[270px] hidden lg:block">sidebar</aside>
+            <aside className="children">
+                <Outlet />
+            </aside>
+        </div>
+    )
+}
+
+export default AdminLayout
